16a7209a8bdcfb67a56849c0425dc7ab47e0ff20
apBUSampK <sdsnm737@gmail.com>
2023-09-05 15:57:28 +0300

    Improved parsing

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import numpy as np
 
 if __name__ == "__main__":
     df = pd.read_csv(input("Enter filename: "), sep=' ', names=['t', 'x', 'y', 'z', 'p0', 'px', 'py', 'pz', 'm', 'ityp',
-                                                                'di3', 'ch', 'pcn', 'ncoll', 'ppt', 'extra'], usecols=range(15))
+                                                                'di3', 'ch', 'pcn', 'ncoll', 'ppt', 'eta', 'nev'])
+    data = pd.DataFrame(columns=df.columns)
     tottime = float(df.iloc[5, 7])
     dtime = float(df.iloc[5, 9])
     time = dtime
 
-    events = []
-    event = []
     seps = df[df['y'].isna()].index
+    nev = 1
 
     # Parse events
 
@@ -20,9 +20,9 @@ if __name__ == "__main__":
         eSlice = eSlice.astype(float)
         p = np.sqrt(np.square(eSlice['px']) + np.square(eSlice['py']) + np.square(eSlice['pz']))
         eSlice['eta'] = np.log((p + eSlice['pz']) / (p - eSlice['pz']))
-        event.append(eSlice)
+        eSlice['nev'] = nev
+        data = pd.concat((data, eSlice), ignore_index=True)
         time += dtime
         if time > tottime:
-            events.append(event.copy())
-            event = []
             time = dtime
+            nev += 1
