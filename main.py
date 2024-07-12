92d1639b1605eac3cd58e0dcbc5ad02201a2de37
apBUS_amp_K <sdsnm737@gmail.com>
2024-07-12 13:40:09 +0300

    rebasing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -21,14 +22,26 @@ nev = 1
 
 # Parse events
 
-for sep in seps:
-    eSlice: pd.DataFrame = df14.iloc[sep + 2 : sep + 2 + int(df14.iloc[sep]['t'])]
-    eSlice = eSlice.astype(float)
-    p = np.sqrt(np.square(eSlice['px']) + np.square(eSlice['py']) + np.square(eSlice['pz']))
-    eSlice['eta'] = np.log((p + eSlice['pz']) / (p - eSlice['pz']))
-    eSlice['nev'] = nev
-    data14 = pd.concat((data14, eSlice), ignore_index=True)
-    time += dtime
-    if time > tottime:
-        time = dtime
-        nev += 1
+    for sep in seps:
+        eSlice: pd.DataFrame = df.iloc[sep + 2 : sep + 2 + int(df.iloc[sep]['t'])]
+        eSlice = eSlice.astype(float)
+        p = np.sqrt(np.square(eSlice['px']) + np.square(eSlice['py']) + np.square(eSlice['pz']))
+        eSlice['eta'] = np.log((p + eSlice['pz']) / (p - eSlice['pz']))
+        eSlice['nev'] = nev
+        data = pd.concat((data, eSlice), ignore_index=True)
+        time += dtime
+        if time > tottime:
+            time = dtime
+            nev += 1
+
+    # Some plots
+
+    fig : plt.Figure = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    d1 = data[1 == data['nev']]
+    for time in d1['t'].unique():
+        d1t = d1[d1['t'] == time]
+        ax.scatter3D(d1t['x'], d1t['y'], d1t['z'], marker='o')
+        plt.savefig(f'./pics/passing_{time}_fmc.png', dpi=300)
+        ax.clear()
